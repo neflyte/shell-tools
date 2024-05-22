e01555888e41257a628ab17aab989d2299ba3d50
alan lew <alan@ethereal.cc>
2024-05-21 22:18:38 -0400

    Add parameters to slimserver build script

--- scripts/build-slimserver.ps1
+++ scripts/build-slimserver.ps1
@@ -4,19 +4,25 @@
 #>
 [CmdletBinding(SupportsShouldProcess)]
 param(
-    [string]$RootDir = "${PWD}"
+    [string]$RootDir = $PWD,
+    [switch]$Build,
+    [string]$DBDir = '/var/db/squeezeboxserver',
+    [string]$LogDir = '/var/log/squeezeboxserver',
+    [string]$Destination = '/opt/squeezeboxserver',
+    [string]$ServiceOwner = 'slimserv',
+    [string]$ServiceGroup = 'slimserv'
 )
 
 <# Define a set of 'constants' #>
 $slimDir = Join-Path $RootDir 'src' 'slimserver'
 $slimVendorDir = Join-Path $RootDir 'src' 'slimserver-vendor','CPAN'
 $slimVendorBuildDir = Join-Path $slimVendorDir 'build'
-$slimDBDir = '/var/db/squeezeboxserver'
+$slimDBDir = $DBDir
 $slimCacheDir = Join-Path $slimDBDir 'cache'
 $slimPlaylistsDir = Join-Path $slimDBDir 'playlists'
 $slimPrefsDir = Join-Path $slimDBDir 'prefs'
-$slimLogDir = '/var/log/squeezeboxserver'
-$destDir = '/opt/squeezeboxserver'
+$slimLogDir = $LogDir
+$destDir = $Destination
 $destCPANDir = Join-Path $destDir 'CPAN'
 $destCacheDir = Join-Path $destDir 'Cache'
 $destPlaylistsDir = Join-Path $destDir 'playlists'
@@ -28,15 +34,32 @@ $symlinkDirs = @{
     $slimPrefsDir = $destPrefsDir
     $slimLogDir = $destLogDir
 }
-$slimservUser = 'slimserv'
-$slimservGroup = 'slimserv'
+$slimservUser = $ServiceOwner
+$slimservGroup = $ServiceGroup
 
 function ensureSourcePaths {
     [CmdletBinding(SupportsShouldProcess)]
     param()
     Write-Host 'Ensure source paths exist'
-    $paths = @($slimDBDir, $slimCacheDir, $slimPlaylistsDir, $slimPrefsDir, $slimLogDir)
+    $paths = $slimDBDir, $slimCacheDir, $slimPlaylistsDir, $slimPrefsDir, $slimLogDir
     foreach ($path in $paths) {
+        Write-Host ".  ${path}"
+        if (-not(Test-Path $path)) {
+            $null = New-Item $path -ItemType Directory -Force -ErrorAction Continue
+            if ($PSCmdlet.ShouldProcess($path, 'Set Owner and Group')) {
+                chown -R "${slimservUser}:${slimservGroup}" "${path}"
+            }
+        }
+    }
+}
+
+function ensureDestPaths {
+    [CmdletBinding(SupportsShouldProcess)]
+    param()
+    Write-Host 'Ensure destination paths exist'
+    $paths = $destDir, $destCPANDir
+    foreach ($path in $paths) {
+        Write-Host ".  ${path}"
         if (-not(Test-Path $path)) {
             $null = New-Item $path -ItemType Directory -Force -ErrorAction Continue
             if ($PSCmdlet.ShouldProcess($path, 'Set Owner and Group')) {
@@ -49,6 +72,7 @@ function ensureSourcePaths {
 function buildVendor {
     [CmdletBinding(SupportsShouldProcess)]
     param()
+    Write-Host "Build vendor libraries in ${slimVendorDir}"
     if (Test-Path $slimVendorDir) {
         if (Test-Path $slimVendorBuildDir) {
             Write-Host 'Remove existing build output directory'
@@ -68,7 +92,7 @@ function buildVendor {
 
 function copyServer {
     if (Test-Path $slimDir) {
-        Write-Host 'Copy squeezeboxserver'
+        Write-Host "Copy squeezeboxserver to ${destDir}"
         Copy-Item -Path "${slimDir}/*" -Destination $destDir -Exclude '.git','.github' -Recurse -Force
     }
 }
@@ -90,6 +114,7 @@ function perlVersion {
 function copyVendor {
     [CmdletBinding(SupportsShouldProcess)]
     param()
+    Write-Host "Copy vendor library files from ${slimVendorBuildDir}"
     if (Test-Path $slimVendorBuildDir) {
         Write-Host 'Copy vendor libraries'
         Copy-Item (Join-Path $slimVendorBuildDir 'arch') $destCPANDir -Recurse -Force
@@ -97,7 +122,7 @@ function copyVendor {
         $perlVersionNumber = perlVersion
         $perlLibDir = Join-Path $slimVendorBuildDir $perlVersionNumber 'lib','perl5'
         if (Test-Path $perlLibDir) {
-            Write-Host 'Copy version-specific perl libraries'
+            Write-Host "Copy version-specific perl libraries from ${perlLibDir}"
             $destCPANLibDir = Join-Path $destCPANDir 'arch' $perlVersionNumber
             if (-not(Test-Path $destCPANLibDir)) {
                 $null = New-Item $destCPANLibDir -ItemType Directory -Force
@@ -112,6 +137,7 @@ function removeSymlinks {
     param()
     Write-Host 'Remove existing symlinks'
     foreach ($sourceDir in $symlinkDirs.Keys) {
+        Write-Host ".  ${sourceDir}"
         if (Test-Path $symlinkDirs.$sourceDir) {
             $dirItem = Get-Item $symlinkDirs.$sourceDir
             if ($null -eq $dirItem.LinkTarget) {
@@ -132,6 +158,7 @@ function createSymlinks {
     param()
     Write-Host 'Create symlinks'
     foreach ($sourceDir in $symlinkDirs.Keys) {
+        Write-Host ".  ${sourceDir}"
         if (-not(Test-Path $sourceDir) -and -not($WhatIfPreference)) {
             throw "link source ${sourceDir} doesn't exist"
         }
@@ -152,11 +179,15 @@ function setOwnership {
     }
 }
 
-ensureSourcePaths
-buildVendor
-removeSymlinks
-copyServer
-copyVendor
-createSymlinks
-setOwnership
+if ($Build) {
+    buildVendor
+} else {
+    ensureSourcePaths
+    ensureDestPaths
+    removeSymlinks
+    copyServer
+    copyVendor
+    createSymlinks
+    setOwnership
+}
 Write-Host 'done.'
