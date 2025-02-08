56e3520332da4d11ffe430fa045c77898247276e
alan lew <alan@ethereal.cc>
2025-02-08 10:33:08 -0500

    Fix string check in nVidia powermizer script

--- scripts/Set-NVidiaPowerMizer.ps1
+++ scripts/Set-NVidiaPowerMizer.ps1
@@ -2,14 +2,14 @@ param()
 $powerMizerModeValue = '0'
 $connectedDisplaysMatches = nvidia-settings -d -q '[gpu:0]/ConnectedDisplays' 2>&1 | Select-String "ConnectedDisplays. \(.*\):(.*)"
 $connectedDisplays = $connectedDisplaysMatches.Matches[0].Groups[1].ToString().Trim()
-if ($connectedDisplays -ne '' -and $connnectedDisplays -ne '.') {
+if ($null -ne $connectedDisplays -and -not($connectedDisplays.Equals('.')) -and -not($connectedDisplays.Equals(''))) {
     $powerMizerModeValue = '1'
 }
 $powerMizerModeSetting = "[gpu:0]/GpuPowerMizerMode=${powerMizerModeValue}"
 Write-Verbose "PS> nvidia-settings -a `"${powerMizerModeSetting}`""
 $output = nvidia-settings -a "${powerMizerModeSetting}"
 if (-not($?)) {
-    Write-Verbose "could not set nvidia setting ${powerMizerModeSetting}"
+    Write-Error "could not set nvidia setting ${powerMizerModeSetting}"
     exit 1
 }
 Write-Verbose "set powermizer mode successfully; output: ${output}"
