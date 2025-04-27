e36e6a86e2a709791a24467087cdc3b1ff122195
alan lew <alan@ethereal.cc>
2025-04-27 09:11:25 -0400

    Add function to convert FLAC to ALAC using ffmpeg

--- modules/ShellTools/MediaFunctions.ps1
+++ modules/ShellTools/MediaFunctions.ps1
@@ -14,4 +14,41 @@ function Get-MediaDump {
     yt-dlp --no-playlist --embed-metadata --embed-thumbnail --write-thumbnail --all-formats --add-metadata --no-overwrites -o '%(id)s_%(format)s.%(ext)s' $args
 }
 
-Export-ModuleMember -Function Get-WebVideo,Get-WebAudio,Get-WebAudioPlaylist,Get-MediaDump
+function Convert-FlacToAlac {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,Position=0)][String]$InFile,
+        [switch]$Force
+    )
+    if (-not(Test-Path $InFile)) {
+        throw "Input file ${InFile} does not exist"
+    }
+    $inFileItem = Get-Item $InFile
+    $outFilePath = $inFileItem.Directory.FullName
+    $outFile = Join-Path $outFilePath "$($inFileItem.BaseName).m4a"
+    Write-Debug "outFilePath=${outFilePath}, outFile=${outFile}"
+    if (Test-Path $outFile) {
+        if (-not($Force)) {
+            throw "Output file ${outFile} exists; use -Force to overwrite it"
+        }
+        Remove-Item $outFile -Force
+    }
+    $ffmpegArgs = @(
+        '-v', 'warning',
+        '-i', $InFile,
+        '-vcodec', 'copy',
+        '-acodec', 'alac',
+        '-map_metadata', '0',
+        $outFile
+    )
+    Write-Host "$($inFileItem.FullName)"
+    Write-Verbose "Convert $($inFileItem.FullName) to ${outFile}"
+    Write-Debug "PS> ffmpeg ${ffmpegArgs}"
+    ffmpeg $ffmpegArgs
+    $returnCode = $LASTEXITCODE
+    if ($returnCode -ne 0) {
+        throw "Error converting FLAC to ALAC; returnCode=${returnCode}"
+    }
+}
+
+Export-ModuleMember -Function Get-WebVideo,Get-WebAudio,Get-WebAudioPlaylist,Get-MediaDump,Convert-FlacToAlac
