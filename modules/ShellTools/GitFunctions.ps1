9489184100bf8a7052fc5a54c786d7ff3969941a
alan lew <alan@ethereal.cc>
2026-04-04 11:00:05 -0400

    Modify git functions

--- modules/ShellTools/GitFunctions.ps1
+++ modules/ShellTools/GitFunctions.ps1
@@ -2,49 +2,50 @@ Function Get-GitBranch {
     [OutputType([string])]
     param()
     $ref = git symbolic-ref --short HEAD 2>$null
-    if (-not($?) -or $ref -eq '') {
+    if ($LASTEXITCODE -ne 0 -or [string]::IsNullOrEmpty($ref)) {
         $ref = git describe --tags --always 2>$null
     }
     return $ref
 }
 
-Function Get-GitBranches {
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
+function Get-GitBranches {
+    param()
     git branch $args
 }
 
-Function Get-GitRepoStatus {
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
-    git status
+function Get-GitRepoStatus {
+    param()
+    git status $args
 }
 
-Function Get-GitTags {
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
+function Get-GitTags {
+    param()
     git tag $args
 }
 
-Function Reset-GitBranch {
+function Reset-GitBranch {
+    param()
     $gitBranch = Get-GitBranch
-    if ($gitBranch -eq '') {
-        throw 'cannot determine the current branch'
+    if ([string]::IsNullOrEmpty($gitBranch)) {
+        Write-Error 'cannot determine the current branch'
+        return
     }
     Write-Host "hard resetting to ${gitBranch}"
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
     git reset --hard "${gitBranch}"
 }
 
-Function Set-GitBranch {
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
+function Set-GitBranch {
+    param()
     git checkout $args
 }
 
-Function Update-GitOriginRepo {
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
+function Update-GitOriginRepo {
+    param()
     git fetch origin $args
 }
 
-Function Update-GitRepo {
-    if ($IsWindows) { $env:GIT_REDIRECT_STDERR = '2>&1' }
+function Update-GitRepo {
+    param()
     git pull $args
 }
 
@@ -53,11 +54,50 @@ function Test-GitRepo {
     param(
         [Parameter(Position=0)][String]$Directory = $PWD
     )
-    $repoDir = Find-DirectoryFromParent -Start $Directory -Directory '.git'
-    if ($null -eq $repoDir) {
+    $repoDir = Find-DirectoryFromParent -Start $Directory -Directory '.git' -ErrorAction SilentlyContinue
+    if (-not($?) -or [string]::IsNullOrEmpty($repoDir)) {
         return $false
     }
     return $true
 }
 
-Export-ModuleMember -Function 'Get-GitBranch','Get-GitBranches','Get-GitRepoStatus','Get-GitTags','Reset-GitBranch','Set-GitBranch','Update-GitOriginRepo','Update-GitRepo','Test-GitRepo'
+function Set-LocationToGitWorktree {
+    [OutputType([void])]
+    param(
+        [Parameter(Mandatory,Position=0)][String]$Directory
+    )
+    if (-not(Test-Path $Directory)) {
+        Write-Error "directory ${Directory} does not exist"
+        return
+    }
+    $wtSelect = Join-Path $env:TOOLS_HOME 'apps' 'wtselect' 'wtselect'
+    if ($IsWindows) {
+        $wtSelect += '.exe'
+    }
+    if (-not(Test-Path $wtSelect)) {
+        Write-Error "app wtselect could not be found at ${wtSelect}"
+        return
+    }
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    try {
+        & "${wtSelect}" "${Directory}" 2>"${tempFile}"
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "wtselect failed with exit code ${LASTEXITCODE}"
+            return
+        }
+        $desiredDir = (Get-Content $tempFile -Raw).Trim()
+        if ([string]::IsNullOrEmpty($desiredDir)) {
+            Write-Verbose 'no directory selected; nothing to do'
+            return
+        }
+        Set-Location $desiredDir
+    } finally {
+        Remove-Item $tempFile -Force -ErrorAction SilentlyContinue
+    }
+}
+
+Export-ModuleMember -Function @(
+    'Get-GitBranch','Get-GitBranches','Get-GitRepoStatus','Get-GitTags',
+    'Reset-GitBranch','Set-GitBranch','Update-GitOriginRepo','Update-GitRepo','Test-GitRepo',
+    'Set-LocationToGitWorktree'
+)
